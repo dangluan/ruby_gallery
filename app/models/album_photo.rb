920b6d6522327340bda45cda5f5a4bd6473bf046
dangluan <peter@rubify.com>
2013-06-15 12:26:54 +0700

    add style standard for album photo

--- app/models/album_photo.rb
+++ app/models/album_photo.rb
@@ -1,5 +1,5 @@
 class AlbumPhoto < ActiveRecord::Base
   attr_accessible :photo
   belongs_to :album_photoable, polymorphic: true
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, url: "/system/:attachment/:id/:style/:filename"
+  has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "100x100#", :standard => "500x300#" }, url: "/system/:attachment/:id/:style/:filename"
 end
